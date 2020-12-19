c0ac07d181faa261d22b5bd03a988e2ba8ef4a98
Qing Ma <gsmofgsm@hotmail.com>
2020-12-19 16:35:59 +0100

    submitForm in livewire wire:model wire:submit

--- app/Http/Livewire/ContactForm.php
+++ app/Http/Livewire/ContactForm.php
@@ -3,9 +3,35 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Mail\ContactFormMailable;
+use Illuminate\Support\Facades\Mail;
 
 class ContactForm extends Component
 {
+    public $name;
+    public $email;
+    public $phone;
+    public $message;
+
+    public function submitForm()
+    {
+//        $contact = $request->validate([
+//            'name' => 'required',
+//            'email' => 'required|email',
+//            'phone' => 'required',
+//            'message' => 'required',
+//        ]);
+
+        $contact['name'] = $this->name;
+        $contact['email'] = $this->email;
+        $contact['phone'] = $this->phone;
+        $contact['message'] = $this->message;
+
+        Mail::to('qing@qing.coom')->send(new ContactFormMailable($contact));
+
+//        return back()->with('success_message', 'We received your message successfully and will get back to you shortly!');
+    }
+
     public function render()
     {
         return view('livewire.contact-form');
