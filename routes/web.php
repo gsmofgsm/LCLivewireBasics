6451621679acb526ce74849d993ab9133afb9f32
Qing Ma <gsmofgsm@hotmail.com>
2020-12-19 13:32:05 +0100

    examples

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/examples', function () {
+    $successMessage = '';
+    return view('examples', compact('successMessage'));
+});
