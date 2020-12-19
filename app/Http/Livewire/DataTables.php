f2bb9975e4fd9e2c9dd2677e0f5d6b36e2e5f44b
Qing Ma <gsmofgsm@hotmail.com>
2020-12-19 22:51:41 +0100

    withPagination

--- app/Http/Livewire/DataTables.php
+++ app/Http/Livewire/DataTables.php
@@ -4,9 +4,12 @@ namespace App\Http\Livewire;
 
 use App\Models\User;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class DataTables extends Component
 {
+    use withPagination;
+
     public function render()
     {
         return view('livewire.data-tables', [
