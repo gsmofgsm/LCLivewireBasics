e5c7368248ee3f86cb5da39ef40e9325f2ae980c
Qing Ma <gsmofgsm@hotmail.com>
2020-12-20 14:49:03 +0100

    livewire.emit on frontend and  on backend

--- app/Http/Livewire/TagsComponent.php
+++ app/Http/Livewire/TagsComponent.php
@@ -9,6 +9,11 @@ class TagsComponent extends Component
 {
     public $tags;
 
+//    protected $listeners = ['postAdded' => 'incrementPostCount'];
+    // if method name is the same
+    // no key value pair needed
+    protected $listeners = ['tagAdded', 'tagRemoved'];
+
     public function mount()
     {
         $allTags = Tag::all();
@@ -18,5 +23,15 @@ class TagsComponent extends Component
     public function render()
     {
         return view('livewire.tags-component');
+    }
+
+    public function tagAdded($tag)
+    {
+        Tag::create(['name' => $tag]);
+    }
+
+    public function tagRemoved($tag)
+    {
+        Tag::where('name', $tag)->first()->delete();
     }
 }
--- resources/views/livewire/tags-component.blade.php
+++ resources/views/livewire/tags-component.blade.php
@@ -1,14 +1,15 @@
 <div
     class="w-1/2 border px-4 py-2"
+    wire:ignore
     x-data
     x-init="
                     new Taggle($el, {
                         tags: {{ $tags }},
                         onTagAdd: function(e, tag) {
-                            console.log('You just added ' + tag + '');
+                            livewire.emit('tagAdded', tag);
                         },
                         onTagRemove: function(e, tag) {
-                            console.log('You just removed ' + tag + '');
+                            livewire.emit('tagRemoved', tag);
                         }
                     })
                 "
